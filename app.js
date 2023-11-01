0fa955cc65eb9947584dcd0e83a08d9f1e93bcd2
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-01 10:07:12 +0700

    Add start script

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-require('dotenv').config();
-
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-const cookieParser = require('cookie-parser');
-const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+require("dotenv").config("./");
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // database connection
 const dbURI = process.env.dbURI;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then((result) => {
     app.listen(process.env.PORT);
-    console.log('connected');
+    console.log("connected");
   })
   .catch((err) => console.log(err));
 
 // routes
-app.get('*', checkUser);
-app.get('/', (req, res) => res.render('home', {
-  test: 'testing'
-}));
-app.get('/index', requireAuth, (req, res) => res.render('index'));
+app.get("*", checkUser);
+app.get("/", (req, res) =>
+  res.render("home", {
+    test: "testing",
+  })
+);
+app.get("/index", requireAuth, (req, res) => res.render("index"));
 app.use(authRoutes);
