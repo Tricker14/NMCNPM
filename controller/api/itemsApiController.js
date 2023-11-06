8981f68dbf393173090199cb7002ea68b6419e8d
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-06 19:37:02 +0700

    Finish item creation with preview images

--- controller/api/itemsApiController.js
+++ controller/api/itemsApiController.js
@@ -7,27 +7,33 @@ module.exports.item_post = async function (req, res) {
   const { name, description, date, category, startingBid } = req.body;
 
   const images = req.files;
-  console.log(images);
-  // const highestBid = startingBid;
-  // const image = req.file.path;
-  // const owner = res.locals.user;
-  // const winner = null;
-  // try {
-  //   const item = await Item.create({
-  //     name,
-  //     description,
-  //     date,
-  //     category,
-  //     startingBid,
-  //     highestBid,
-  //     image,
-  //     owner,
-  //     winner,
-  //   });
-  //   res.status(201).json({ item, redirect: "/items" });
-  // } catch (err) {
-  //   const errors = handleItemErrors(err);
-  //   res.status(400).json({ errors });
-  // }
-  res.send("under maintenance");
+  let image = null;
+  let previewImages = [];
+
+  image = Object.values(images)[0][0].filename;
+
+  Object.values(images)[1].forEach((preview) => {
+    previewImages.push(preview.filename);
+  });
+  const highestBid = startingBid;
+  const owner = res.locals.user;
+  const winner = null;
+  try {
+    const item = await Item.create({
+      name,
+      description,
+      date,
+      category,
+      startingBid,
+      highestBid,
+      image,
+      previewImages,
+      owner,
+      winner,
+    });
+    res.status(201).json({ item, redirect: "/items" });
+  } catch (err) {
+    const errors = handleItemErrors(err);
+    res.status(400).json({ errors });
+  }
 };
--- middleware/fileUploadMiddleware.js
+++ middleware/fileUploadMiddleware.js
@@ -15,48 +15,4 @@ const storage = multer.diskStorage({
 // Create multer upload instance
 const upload = multer({ storage: storage });
 
-// Custom file upload middleware
-const uploadMiddleware = (req, res, next) => {
-  // Use multer upload instance
-  upload.array("previewImages", 5)(req, res, (err) => {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-
-    // Retrieve uploaded files
-    const files = req.files;
-    const errors = [];
-
-    // Validate file types and sizes
-    files.forEach((file) => {
-      const allowedTypes = ["image/jpeg", "image/png"];
-      const maxSize = 5 * 1024 * 1024; // 5MB
-
-      if (!allowedTypes.includes(file.mimetype)) {
-        errors.push(`Invalid file type: ${file.originalname}`);
-      }
-
-      if (file.size > maxSize) {
-        errors.push(`File too large: ${file.originalname}`);
-      }
-    });
-
-    // Handle validation errors
-    if (errors.length > 0) {
-      // Remove uploaded files
-      files.forEach((file) => {
-        fs.unlinkSync(file.path);
-      });
-
-      return res.status(400).json({ errors });
-    }
-
-    // Attach files to the request object
-    req.previewImages = files;
-
-    // Proceed to the next middleware or route handler
-    next();
-  });
-};
-
-module.exports = { uploadMiddleware, upload };
+module.exports = { upload };
