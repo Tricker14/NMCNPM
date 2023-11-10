a07b4eb08de94ff841335f4c503eca71834a361a
Tricker14 <trietthetricker@gmail.com>
2023-11-10 09:48:44 +0700

    update product price when user place a bid

--- controller/api/bidsApiController.js
+++ controller/api/bidsApiController.js
@@ -1,12 +1,21 @@
 const Bid = require("../../models/bid");
+const Item = require("../../models/item");
+const User = require("../../models/user");
 
 module.exports.bid_post = async function(req, res){
-    const { price, product, bidder } = req.body;
+    const product = await Item.findById(req.params._id);
+    const bidder = res.locals.user;
+    const price = req.body.price;
     try{
-        let currentPrice = product.
-        if(price );
-        const bid = await Bid.create({ price, product, bidder });
-        res.status(201).json({bid});
+        if(price > product.highestBid){
+            const bid = await Bid.create({ price, product, bidder });
+            product.highestBid = price;
+            await product.save();
+            res.status(201).json({bid});
+        }
+        else{
+            res.status(400).json(`You must bid higher than ${product.highestBid}`);
+        }
     }
     catch(err){
         console.log("error", err);
--- routes/api/bidsApi.js
+++ routes/api/bidsApi.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const bidsApiController = require("../../controller/api/bidsApiController");
 
-router.post("/bids", bidsApiController.bid_post);
+router.post("/bids/:_id", bidsApiController.bid_post);
 // router.delete("/bids", bidsApiController.bid_delete);
 module.exports = router;
