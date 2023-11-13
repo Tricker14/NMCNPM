055ee49a3ca572b72217fa7216017576fc48563a
Do Minh Triet <116749322+Tricker14@users.noreply.github.com>
2023-11-14 01:11:24 +0700

    Update itemsApiController.js

--- controller/api/itemsApiController.js
+++ controller/api/itemsApiController.js
@@ -35,9 +35,8 @@ module.exports.item_post = async function (req, res) {
     });
     res.status(201).json({ item, redirect: "/items" });
   } catch (err) {
-    // const errors = handleItemErrors(err);
-    // res.status(400).json({ errors });
-    res.status(400).json({ err });
+    const errors = handleItemErrors(err);
+    res.status(400).json({ errors });
   }
 };
 
