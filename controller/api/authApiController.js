539f788ecf917a22382712bdbfec0e1abef35fc4
Tricker14 <trietthetricker@gmail.com>
2024-02-24 03:35:10 +0700

    add callbackURL to .env

--- controller/api/authApiController.js
+++ controller/api/authApiController.js
@@ -102,7 +102,7 @@ passport.deserializeUser(async function(id, done){
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "/api/webid/auth/google/callback"
+  callbackURL: process.env.LOCAL_URL
 },
 async function(accessToken, refreshToken, profile, done){
   const user = await User.findOne({googleID: profile.id});
